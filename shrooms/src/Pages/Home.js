e8713378dae216d81e05bc316329589440805612
skwidhubz <tommaxwellparker@gmail.com>
2023-06-04 08:48:01 +1000

    react init

---
+++ shrooms/src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import 'react-bootstrap'
+
+const Home = () => {
+
+
+return (
+    <>
+    <div>
+        this is a home page
+    </div>
+    </>
+)
+
+}
+
+export default Home; 
